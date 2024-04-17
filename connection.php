4ab2b5b14ea9cc32a50e9c933041413c5c0a14ee
ᴊᴀʜɪᴅ <84239550+zenjahid@users.noreply.github.com>
2024-04-18 00:49:44 +0600

    update

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
 <?php
 class Teledoc {
     public static function connect() {
-        try {  
-            $con = new PDO('mysql:host=localhost;dbname=teledoc;port=3308', 'root', '');
+        try {
+            $db_name = "teledoc";
+            $localhost = "localhost";
+            $username = "root";
+            $password = "";
+            // $port = 3307; // 
+            $port = 3308; // comment this for normal useage and uncomment the above line 
+
+            $con = new PDO("mysql:host=$localhost;dbname=$db_name;port=$port", $username, $password);
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $con; // Return the PDO connection object
         } catch(PDOException $e) {
